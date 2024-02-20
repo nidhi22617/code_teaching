f875fbdd0a28176e53c3499176bf81840df8fe58
nidhi7733 <pnidhi2@gmail.com>
2024-02-20 11:18:40 +0545

    first commit

---
+++ C++_Programs/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout<<"Enter the first Number: ";
+    cin>>num1;
+    cout<<"Enter the Second Number: ";
+    cin>>num2;
+    int sum = num1 + num2;
+    cout<<"The Sum of two given number is: "<<sum;
+
+    return 0;
+}
---
+++ Java_programs/HelloWorld.java
@@ -0,0 +1,10 @@
+// Declares the HelloWorld class
+public class HelloWorld {
+    
+    // Main method - program execution starts here
+    public static void main(String[] args) {
+        
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
